42ad138423206bdcd2c474f8c5cb16871dc9827e
Patrick Sturm <p.sturm@spherical-elephant.com>
2018-02-21 14:00:15 +0100

    added buttons to illustrate profiling capabilities

--- app/src/main/java/com/sphericalelephant/example/androidtesting/activity/MainActivity.java
+++ app/src/main/java/com/sphericalelephant/example/androidtesting/activity/MainActivity.java
@@ -4,11 +4,18 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.sphericalelephant.example.androidtesting.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by siyb on 24/11/15.
@@ -21,25 +28,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        RequestQueue queue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_mainactivity);
         final SharedPreferences sp = getSharedPreferences(MainActivity.SHAREDPREFERENCES, Context.MODE_PRIVATE);
 
         final EditText et = (EditText) findViewById(R.id.activity_mainactivity_et_input1);
 
         // clicking the TextView changes its content
-        findViewById(R.id.activity_mainactivity_tv_textview).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((TextView) v).setText(R.string.activity_mainactivity_textviewclicked);
-            }
+        findViewById(R.id.activity_mainactivity_tv_textview).setOnClickListener(v -> {
+            ((TextView) v).setText(R.string.activity_mainactivity_textviewclicked);
         });
 
         // this button's listener will write the content of the EditText to the SharedPreferences
-        findViewById(R.id.activity_mainactivity_b_normalbutton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sp.edit().putString(SHARED_PREFERENCES_KEY, et.getText().toString()).apply();
-            }
+        findViewById(R.id.activity_mainactivity_b_normalbutton).setOnClickListener(v -> {
+            sp.edit().putString(SHARED_PREFERENCES_KEY, et.getText().toString()).apply();
         });
+
+        findViewById(R.id.activity_mainactivity_b_cpuload).setOnClickListener(v -> {
+            v.setEnabled(false);
+            for (int i = 0; i < 100000000; i++) {
+                Math.sqrt(i);
+            }
+            v.setEnabled(true);
+        });
+        findViewById(R.id.activity_mainactivity_b_memory).setOnClickListener(v -> {
+            v.setEnabled(false);
+            List<Object> l = new ArrayList<?>(10000);
+            for (int i = 0; i < 100000; i++) {
+                l.add(new Object());
+            }
+            v.setEnabled(true);
+        });
+        findViewById(R.id.activity_mainactivity_b_network).setOnClickListener(v -> {
+            v.setEnabled(false);
+            queue.add(new StringRequest(Request.Method.GET, "http://www.google.com",
+                    response -> {
+                        Toast.makeText(this, response, Toast.LENGTH_SHORT).show();
+                        v.setEnabled(true);
+                    },
+                    error -> {
+                        Toast.makeText(this, error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        v.setEnabled(true);
+                    }
+            ));
+        });
+
     }
 }
